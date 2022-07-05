ae3e9a47d8d0470fccf736c6b4b2cd6e4032483e
Helio Chissini de Castro <heliocastro@gmail.com>
2022-07-05 13:49:54 +0200

    bug(hugo): Create am empty directory for theme otherwise Hugo mod fails
    Signed-off-by: Helio Chissini de Castro <heliocastro@gmail.com>

--- docker_serve_local.sh
+++ docker_serve_local.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-echo "Open local browser on port 1313"
+echo "Create a template dir to allow Hugo properly clone the modules"
+mkdir -p themes/docsy
 
+echo "Open local browser on port 1313"
 docker run -v $PWD:/src --rm --name sw360_website -p 1313:1313 klakegg/hugo:0.93.2-ext-ubuntu server --verbose --verboseLog --debug --cleanDestinationDir --baseUrl http://localhost:1313/sw360
