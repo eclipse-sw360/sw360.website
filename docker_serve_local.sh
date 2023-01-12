60ffcc399322e90b984c3c90ee82986999a7e9cd
Helio Chissini de Castro <heliocastro@gmail.com>
2023-01-12 11:02:49 +0100

    fix(localdev): Add parameters to local service
    Signed-off-by: Helio Chissini de Castro <heliocastro@gmail.com>

--- docker_serve_local.sh
+++ docker_serve_local.sh
@@ -4,4 +4,4 @@ echo "Create a template dir to allow Hugo properly clone the modules"
 mkdir -p themes/docsy
 
 echo "Open local browser on port 1313"
-docker run -v $PWD:/src --rm --name sw360_website -p 1313:1313 klakegg/hugo:0.93.2-ext-ubuntu server --verbose --verboseLog --debug --cleanDestinationDir --baseUrl http://localhost:1313/sw360
+docker run -v $PWD:/src --rm --name sw360_website -p 1313:1313 $@ klakegg/hugo:0.93.2-ext-ubuntu server --verbose --verboseLog --debug --cleanDestinationDir --baseUrl http://localhost:1313/sw360
